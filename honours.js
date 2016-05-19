c76d0884b80fff94a109966a31a7032f59ff8f44
mlio <mercy.lio@stud-inf.unibz.it>
2016-05-19 11:31:16 +0200

    testing charts.js

--- honours.js
+++ honours.js
@@ -21,3 +21,27 @@ var myChart = new Chart(ctx, {
    }
  });
 });
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart2");
+var myChart2 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', 'Blue', "Yellow", "Green", "Purple", "Orange"],
+     datasets: [{
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
