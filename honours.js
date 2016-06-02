62eb2b117fe1054b038dd924228fadc88055fbb9
mlio <mercy.lio@stud-inf.unibz.it>
2016-06-02 13:48:07 +0200

    Fixes

--- honours.js
+++ honours.js
@@ -50,3 +50,30 @@ var myChart2 = new Chart(ctx, {
    }
  });
 });
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart3");
+var myChart3 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
