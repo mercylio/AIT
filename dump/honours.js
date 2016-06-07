ff881535851b034c981d22a61efb2f955c62360b
mlio <mercy.lio@stud-inf.unibz.it>
2016-06-07 11:26:25 +0200

    Cleaning up

---
+++ dump/honours.js
@@ -0,0 +1,274 @@
+$(document).ready(function() {
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB"],
+     labels: ['Red', 'Blue', "Yellow", "Green", "Purple", "Orange"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB"],
+      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+       label: '# of Votes',
+       data: [[12, 19, 3, 5, 2, 3]]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart2");
+var myChart2 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', 'Blue', "Yellow", "Green", "Purple", "Orange"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB"],
+      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart3");
+var myChart3 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart4");
+var myChart4 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart5");
+var myChart5 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart6");
+var myChart6 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart7");
+var myChart7 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+$(document).ready(function() {
+var ctx = document.getElementById("myChart8");
+var myChart8 = new Chart(ctx, {
+   type: 'pie',
+   data: {
+     labels: ['Red', "Orange", "Yellow", "Green", 'Blue', 'Indigo', "Purple"],
+     datasets: [{
+      backgroundColor: ["#FF6384", "#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56"],
+      hoverBackgroundColor: ["#4BC0C0", "#FFCE56", "#E7E9ED", "#36A2EB", "#36A2EB", "#FFCE56", "#FF6384"],
+       label: '# of Votes',
+       data: [12, 19, 3, 5, 2, 3, 5]
+     }]
+   },
+   options: {
+     scales: {
+       yAxes: [{
+         ticks: {
+          beginAtZero:true,
+         }
+       }]
+     },
+     responsive:true
+   }
+ });
+});
+
+
+// $(document).ready(function() {
+// var ctx = document.getElementById("myChart8");
+// var myChart8 = new Chart(ctx, {
+//    type: 'pie',
+//    data: {
+//     var jsonData = $ .ajax ({
+//      Url: ' Http: // your server /json.php',
+//      dataType: 'json',
+//     }). Done (function (results) {
+// // It out to specify the column of the table.
+//       var labels = [], data = [], data2 = [], data3 = [];
+//       results [ "ga"]. forEach (function (ga) {
+//         labels.push (parseFloat (ga.DATE));
+//         data.push (parseFloat (ga.hotelA));
+//         data2.push (parseFloat (ga.hotelB));
+//         data3.push (parseFloat (ga.hotelC));
+//       });   
+//       var myBarChartData = {
+//         labels: labels,
+//         datasets: [
+//         {   fillColor: "# 947030",
+//             strokeColor: "# 947030",
+//             pointColor: "rgba (220,220,220,1)",
+//             pointStrokeColor: "# 947030",
+//             pointHighlightFill: "#fff",
+//             pointHighlightStroke: "rgba (151,187,205,1)",
+//             data: data
+//         },
+//         {   fillColor: '# 76AE25',
+//             strokeColor: '# 76AE25',
+//             pointColor: 'rgba (220,220,220,1)',
+//             pointStrokeColor: '# 76AE25',
+//             data: data2
+//         },
+//         {   fillColor: '# FF5F17',
+//             strokeColor: '# FF5F17',
+//             pointColor: 'rgba (120,120,120,1)',
+//             pointStrokeColor: '# FF5F17',
+//             data: data3
+//         }]
+//       };
+//        // .Var ctx3 = document.getElementById("myChart8")getContext( "2d");
+//        //  window.myBar = new Chart(ctx3).Bar(myBarChartData);
+//     });
+//    },
+//    options: {
+//      scales: {
+//        yAxes: [{
+//          ticks: {
+//           beginAtZero:true,
+//          }
+//        }]
+//      },
+//      responsive:true
+//    }
+//  });
+// });
+
